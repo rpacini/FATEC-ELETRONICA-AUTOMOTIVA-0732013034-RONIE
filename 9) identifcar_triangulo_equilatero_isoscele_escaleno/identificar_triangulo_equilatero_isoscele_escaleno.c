99847346affb3f612119b61d8f956daa3d5bea41
rpacini <70667579+rpacini@users.noreply.github.com>
2020-09-23 20:14:49 -0300

    Correção para funcionar

--- 9) identifcar_triangulo_equilatero_isoscele_escaleno/identificar_triangulo_equilatero_isoscele_escaleno.c
+++ 9) identifcar_triangulo_equilatero_isoscele_escaleno/identificar_triangulo_equilatero_isoscele_escaleno.c
@@ -6,7 +6,7 @@
 
 int main(void) {
 
-      int e_lado_1_triangulo, e_lado_2_triangulo, e_lado_3_triangulo, s_triangulo_equilatero, s_triangulo_isoscele, s_triangulo_escaleno;
+      int e_lado_1_triangulo, e_lado_2_triangulo, e_lado_3_triangulo;
 
 printf("Digite o lado 1 do triângulo em número inteiro:\n");
 scanf("%i", &e_lado_1_triangulo);
@@ -18,20 +18,20 @@ printf("Digite o lado 3 do triângulo em número inteiro:\n");
 scanf("%i", &e_lado_3_triangulo);
 
 
-if (e_lado_1_triangulo = e_lado_2_triangulo) {
-      if (e_lado_1_triangulo=e_lado_3_triangulo) {
-          printf("Triângulo equilátero! %i\n", s_triangulo_equilatero);
+if (e_lado_1_triangulo == e_lado_2_triangulo) {
+      if (e_lado_1_triangulo == e_lado_3_triangulo) {
+          printf("Triângulo equilátero!\n");
 
       }else{ 
-          printf("Triângulo Isóscele! %i\n", s_triangulo_isoscele);
+          printf("Triângulo Isóscele!\n");
 
       }
 
 }else{
-        if (e_lado_2_triangulo=e_lado_3_triangulo){
-        printf("Triângulo Isóscele! %i\n", s_triangulo_isoscele);
+        if (e_lado_2_triangulo == e_lado_3_triangulo){
+        printf("Triângulo Isóscele!\n");
         }else{
-          printf("Triângulo Escaleno! %i\n", s_triangulo_escaleno);
+          printf("Triângulo Escaleno!\n");
         }
 }
 
